a8cce8a3445bc4d84f7796a2345a450f7d53c467
DESKTOP-5HBNP4P\DulanW <dulanr.w@outlook.com>
2017-10-13 21:52:13 +0100

    Patrol route implemented with actor components. Removing PatrolGuard class

--- Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -3,17 +3,24 @@
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h" //TODO Remove coupling
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	Super::ExecuteTask(OwnerComp, NodeMemory);
 
 	//Get Patrol Points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	UPatrolRoute* PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	//auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s is missing patrol points!"), *ControlledPawn->GetName());
+		return EBTNodeResult::Failed;
+	}
 
 	//Get Next Way Point Index
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
@@ -22,12 +29,10 @@ EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& Own
 	//Set Patrol Point
 	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[NextWaypointIndex]);
 
-
 	//Cycle Index
 	NextWaypointIndex = (NextWaypointIndex + 1) % PatrolPoints.Num();
 	BlackboardComp->SetValueAsInt(NextWaypointIndexKey.SelectedKeyName, NextWaypointIndex);
 
-	//UE_LOG(LogTemp, Warning, TEXT("Choose Next Waypoint executed! %s"), *OwnerComp.GetOwner()->GetName());
 	return EBTNodeResult::Succeeded;
 }
 
